ae0f997fa5d659e15a84183867c8cfb5c5359dbc
Bob-Coding <bobgunsing@gmail.com>
2019-12-13 12:05:33 +0100

    serialise msg

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 const bodyParser = require("body-parser");
+const Sse = require("json-sse");
 const routerMessage = require("./message/router");
+const stream = new Sse();
+const Message = require("./message/model");
 
+//now that the stream is connected after putting in terminal http :4000/stream --stream it will send hi
 app.get("/", (request, response) => {
+  stream.send("hi");
   response.send("hello");
 });
+//gives the option where clients can connect and you can send multiple responses on one req
+app.get("/stream", async (request, response) => {
+  //show all msgs
+  try {
+    //get array out of database
+    const messages = await Message.findAll();
+    //convert array intro string = 'serialize'
+    const string = JSON.stringify(messages);
+    //prepare string to be send to client right after they *connect*
+    stream.updateInit(string);
 
+    //conect user to stream
+    stream.init(request, response);
+  } catch (error) {
+    next(error); //handle any errors
+  }
+});
+//when users connect to the stream lets them see all msgs that were there
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
 app.use(routerMessage);
--- message/router.js
+++ message/router.js
@@ -4,7 +4,7 @@ const Message = require("./model");
 const { Router } = express;
 const router = new Router();
 // old way:
-// //get messages thats posted
+// get messages thats posted
 // router.get("/message", (request, response, next) => {
 //   Message.findAll()
 //   .then(messages => {
@@ -12,11 +12,6 @@ const router = new Router();
 //  })
 // .catch(next);
 // });
-router.get("/message", async (request, response, next) => {
-  const messages = await Message.findAll();
-
-  response.send(messages);
-});
 // old way:
 // create new msg make a post endpoint
 // router.post("/message", (request, response, next) => {
@@ -27,10 +22,23 @@ router.get("/message", async (request, response, next) => {
 // .catch(next);
 // });
 // the other way:
+router.get("/message", async (request, response, next) => {
+  try {
+    const messages = await Message.findAll();
+
+    response.send(messages);
+  } catch (error) {
+    next(error);
+  }
+});
+
 router.post("/message", async (request, response, next) => {
   const message = await Message.create(request.body);
-
-  response.send(message);
+  try {
+    response.send(message);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
