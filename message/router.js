9ec153ebd06fffbad7a8abfba68c2a0e52b0ece3
Bob-Coding <bobgunsing@gmail.com>
2019-12-13 11:03:17 +0100

    changed post and get in await

--- message/router.js
+++ message/router.js
@@ -1,22 +1,36 @@
 const express = require("express");
 const Message = require("./model");
-const bodyParser = require("body-parser");
+
 const { Router } = express;
 const router = new Router();
-//get messages thats posted
-router.get("/message", (request, response, next) => {
-  Message.findAll()
-    .then(messages => {
-      response.send(messages);
-    })
-    .catch(next);
+// old way:
+// //get messages thats posted
+// router.get("/message", (request, response, next) => {
+//   Message.findAll()
+//   .then(messages => {
+//  response.send(messages);
+//  })
+// .catch(next);
+// });
+router.get("/message", async (request, response, next) => {
+  const messages = await Message.findAll();
+
+  response.send(messages);
 });
-//create new msg make a post endpoint
-router.post("/message", (request, response, next) => {
-  Message.create(request.body)
-    .then(message => {
-      response.send(message);
-    })
-    .catch(next);
+// old way:
+// create new msg make a post endpoint
+// router.post("/message", (request, response, next) => {
+// Message.create(request.body)
+// .then(message => {
+// response.send(message);
+//  })
+// .catch(next);
+// });
+// the other way:
+router.post("/message", async (request, response, next) => {
+  const message = await Message.create(request.body);
+
+  response.send(message);
 });
+
 module.exports = router;
