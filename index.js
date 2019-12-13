d216712c58fdc88c755b16e882ae0ca6d3e648ab
Bob-Coding <bobgunsing@gmail.com>
2019-12-13 10:55:17 +0100

    registered endpoints post and get

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
 const routerMessage = require("./message/router");
 
 app.get("/", (request, response) => {
   response.send("hello");
 });
 
+const jsonParser = bodyParser.json();
+app.use(jsonParser);
 app.use(routerMessage);
+
 app.listen(port, () => {
   console.log(`listening on : ${port}`);
 });
--- message/router.js
+++ message/router.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const Message = require("./model");
+const bodyParser = require("body-parser");
 const { Router } = express;
 const router = new Router();
-
+//get messages thats posted
 router.get("/message", (request, response, next) => {
   Message.findAll()
     .then(messages => {
       response.send(messages);
+    })
+    .catch(next);
+});
+//create new msg make a post endpoint
+router.post("/message", (request, response, next) => {
+  Message.create(request.body)
+    .then(message => {
+      response.send(message);
     })
     .catch(next);
 });
